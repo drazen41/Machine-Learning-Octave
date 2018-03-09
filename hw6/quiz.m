77dfb00e516c35913fe0a9b5693612f064e6df98
drazen41 <drazen.svec@gmail.com>
2018-03-09 15:21:29 +0100

    quiz

---
+++ hw6/quiz.m
@@ -0,0 +1,25 @@
+ 
+ truePositive = 85;
+ falsePositive = 890;
+ falseNegative = 15;
+ trueNegative = 10;
+ totalExamples = truePositive + falsePositive + falseNegative + trueNegative;
+ Accuracy = (truePositive + trueNegative) / totalExamples;
+ Precision = (truePositive)/(truePositive + falsePositive);
+ Recall = (truePositive) / (truePositive + falseNegative);
+ F1 = (2*Precision*Recall)/(Precision + Recall);
+ 
+ % -------------------------------------------------------
+ spam = 10;
+ nospam = 990;
+ y=0;
+ trueNegSpam= 990;
+ falseNegSpam = 10;
+ totalExamSpam = trueNegSpam + falseNegSpam;
+ truePosSpam = 0;
+  falsePosSpam = 0;
+ RecallSpam =  truePosSpam / (truePosSpam + falseNegSpam);
+ AccurSpam = (truePosSpam + trueNegSpam) / totalExamSpam;
+ PrecSpam = (truePosSpam) / (truePosSpam + falsePosSpam);
+ 
+ 
